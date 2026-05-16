6d641c9d0c03f98b181f7554ed1e59c7ba5db78d
BelugaDeveloperYT <roblox041213@gmail.com>
2026-05-16 22:39:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -123,3 +123,49 @@ themeToggle.addEventListener(
   document.body.classList.toggle(
   "light");
 });
+
+// ACTIVE NAVIGATION
+
+const sections =
+document.querySelectorAll("section");
+
+const navLinks =
+document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+  let current = "";
+
+  sections.forEach((section) => {
+
+    const sectionTop =
+    section.offsetTop;
+
+    const sectionHeight =
+    section.clientHeight;
+
+    if (
+      pageYOffset >=
+      sectionTop - 200
+    ) {
+
+      current =
+      section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+
+    link.classList.remove(
+    "active-link");
+
+    if (
+      link.getAttribute("href")
+      === `#${current}`
+    ) {
+
+      link.classList.add(
+      "active-link");
+    }
+  });
+});
