9df191bf2ddb13e63b16808871ba8b616d5945ee
BelugaDeveloperYT <roblox041213@gmail.com>
2026-05-16 13:35:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 console.log("Website loaded!");
+// =========================
+// LOADER
+// =========================
+
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader-wrapper");
+
+  setTimeout(() => {
+    loader.classList.add("fade");
+  }, 800);
+});
+
+// =========================
+// SCROLL REVEAL
+// =========================
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealSections() {
+  reveals.forEach((section) => {
+
+    const windowHeight = window.innerHeight;
+    const revealTop = section.getBoundingClientRect().top;
+
+    const revealPoint = 120;
+
+    if (revealTop < windowHeight - revealPoint) {
+      section.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealSections);
+
+revealSections();
