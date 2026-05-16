80cdbae9359194e609be8935ca26ecd23aa0f7cb
BelugaDeveloperYT <roblox041213@gmail.com>
2026-05-16 13:42:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,125 @@
-console.log("Website loaded!");
-// =========================
 // LOADER
-// =========================
 
 window.addEventListener("load", () => {
-  const loader = document.querySelector(".loader-wrapper");
+
+  const loader =
+  document.querySelector(".loader-wrapper");
 
   setTimeout(() => {
+
     loader.classList.add("fade");
-  }, 800);
+
+  }, 900);
 });
 
-// =========================
 // SCROLL REVEAL
-// =========================
 
-const reveals = document.querySelectorAll(".reveal");
+const reveals =
+document.querySelectorAll(".reveal");
 
 function revealSections() {
+
   reveals.forEach((section) => {
 
-    const windowHeight = window.innerHeight;
-    const revealTop = section.getBoundingClientRect().top;
+    const windowHeight =
+    window.innerHeight;
+
+    const revealTop =
+    section.getBoundingClientRect().top;
 
     const revealPoint = 120;
 
-    if (revealTop < windowHeight - revealPoint) {
+    if (
+      revealTop <
+      windowHeight - revealPoint
+    ) {
+
       section.classList.add("active");
     }
   });
 }
 
-window.addEventListener("scroll", revealSections);
+window.addEventListener(
+"scroll",
+revealSections);
 
 revealSections();
+
+// TYPING EFFECT
+
+const text = [
+
+  "Frontend Developer",
+  "UI Designer",
+  "Creative Coder",
+  "JavaScript Learner"
+];
+
+let count = 0;
+let index = 0;
+
+let currentText = "";
+let letter = "";
+
+(function type() {
+
+  if (count === text.length) {
+    count = 0;
+  }
+
+  currentText = text[count];
+
+  letter =
+  currentText.slice(0, ++index);
+
+  document.querySelector(
+  ".typing-text"
+  ).textContent = letter;
+
+  if (
+    letter.length ===
+    currentText.length
+  ) {
+
+    count++;
+
+    index = 0;
+
+    setTimeout(type, 1200);
+
+  } else {
+
+    setTimeout(type, 100);
+  }
+
+})();
+
+// CURSOR GLOW
+
+const cursorGlow =
+document.querySelector(".cursor-glow");
+
+window.addEventListener(
+"mousemove",
+(e) => {
+
+  cursorGlow.style.left =
+  e.clientX + "px";
+
+  cursorGlow.style.top =
+  e.clientY + "px";
+});
+
+// DARK/LIGHT MODE
+
+const themeToggle =
+document.getElementById(
+"theme-toggle");
+
+themeToggle.addEventListener(
+"click",
+() => {
+
+  document.body.classList.toggle(
+  "light");
+});
